1c2333175b0c129dc0163d16738e3548034d43f7
bewarb <pineapplepower04@gmail.com>
2025-10-23 23:54:48 -0400

    feat(web): add react-query + axios and initial API client setup

---
+++ apps/web/lib/api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+});
+
+api.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("jwt");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err?.response?.status === 401) {
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default api;
